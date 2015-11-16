a6934e1362d27697725336856dc65aabfb3b0fe6
wgli <wgli@Ctrip.com>
2015-11-16 11:20:59 +0800

    LCommon-V1-updat container

--- src/LCommon/LCommon.Autofac/AutofacObjectContainer.cs
+++ src/LCommon/LCommon.Autofac/AutofacObjectContainer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Autofac;
 using LCommon.Components;
 
 namespace LCommon.Autofac
@@ -9,8 +10,115 @@ namespace LCommon.Autofac
     /// <summary>
     /// Autofac implementationi of IObjectContainer.
     /// </summary>
-    public class AutofacObjectContainer
+    public class AutofacObjectContainer : IObjectContainer
     {
+        #region CTOR
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public AutofacObjectContainer()
+        {
+            _container = new ContainerBuilder().Build();
+        }
+
+        /// <summary>
+        /// Parameterized constructor.
+        /// </summary>
+        /// <param name="containerBuilder"></param>
+        public AutofacObjectContainer(ContainerBuilder containerBuilder)
+        {
+            _container = containerBuilder.Build();
+        }
+
+        #endregion
+
+        #region property
+
+        private readonly IContainer _container;
+
+        public IContainer Container
+        {
+            get { return _container; }
+        }
+
+        #endregion
+
+        #region IObjectContainer 成员
+
+        /// <summary>
+        /// Register a implementation type.
+        /// </summary>
+        /// <param name="implementationType">The implementation type.</param>
+        /// <param name="serviceName">The service name.</param>
+        /// <param name="life">The life cycle of the implementer type.</param>
+        public void RegisterType(Type implementationType, string serviceName = null, LifeStyle life = LifeStyle.Singleton)
+        {
+            var builder = new ContainerBuilder();
+
+            var registrationBuilder = builder.RegisterType(implementationType);
+            if (serviceName != null)
+                registrationBuilder.Named(serviceName, implementationType);
+            if (life == LifeStyle.Singleton)
+                registrationBuilder.SingleInstance();
+
+            builder.Update(_container);
+        }
+
+        /// <summary>
+        /// Register a implementer type as a service implementation.
+        /// </summary>
+        /// <param name="serviceType">The service type.</param>
+        /// <param name="implementationType">The implementation type.</param>
+        /// <param name="serviceName">The service name.</param>
+        /// <param name="life"></param>
+        public void RegisterType(Type serviceType, Type implementationType, string serviceName = null, LifeStyle life = LifeStyle.Singleton)
+        {
+            var builder = new ContainerBuilder();
+
+            var registrationBuilder = builder.RegisterType(implementationType).As(serviceType);
+            if (serviceName != null)
+                registrationBuilder.Named(serviceName, implementationType);
+            if (life == LifeStyle.Singleton)
+                registrationBuilder.SingleInstance();
+
+            builder.Update(_container);
+        }
+
+        public void Register<TService, TImplementer>(string serviceName = null, LifeStyle life = LifeStyle.Singleton)
+            where TService : class
+            where TImplementer : class, TService
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterInstance<TService, TImplementer>(TImplementer instance, string serviceName = null)
+            where TService : class
+            where TImplementer : class, TService
+        {
+            throw new NotImplementedException();
+        }
+
+        public TService Resolve<TService>() where TService : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Resolve(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TService ResolveNamed<TService>(string serviceName) where TService : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ResolveNamed(string serviceName, Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
--- src/LCommon/LCommon/Components/ObjectContainer.cs
+++ src/LCommon/LCommon/Components/ObjectContainer.cs
@@ -10,6 +10,96 @@ namespace LCommon.Components
     /// </summary>
     public class ObjectContainer
     {
+        /// <summary>
+        /// Represents the current object container.
+        /// </summary>
+        public static IObjectContainer Current { get; set; }
 
+        /// <summary>
+        /// Set the object container.
+        /// </summary>
+        /// <param name="container"></param>
+        public static void SerContainer(IObjectContainer container)
+        {
+            Current = container;
+        }
+
+        /// <summary>Register a implementation type.
+        /// </summary>
+        /// <param name="implementationType">The implementation type.</param>
+        /// <param name="serviceName">The service name.</param>
+        /// <param name="life">The life cycle of the implementer type.</param>
+        public static void RegisterType(Type implementationType, string serviceName = null, LifeStyle life = LifeStyle.Singleton)
+        {
+            Current.RegisterType(implementationType, serviceName, life);
+        }
+        /// <summary>Register a implementer type as a service implementation.
+        /// </summary>
+        /// <param name="serviceType">The implementation type.</param>
+        /// <param name="implementationType">The implementation type.</param>
+        /// <param name="serviceName">The service name.</param>
+        /// <param name="life">The life cycle of the implementer type.</param>
+        public static void RegisterType(Type serviceType, Type implementationType, string serviceName = null, LifeStyle life = LifeStyle.Singleton)
+        {
+            Current.RegisterType(serviceType, implementationType, serviceName, life);
+        }
+        /// <summary>Register a implementer type as a service implementation.
+        /// </summary>
+        /// <typeparam name="TService">The service type.</typeparam>
+        /// <typeparam name="TImplementer">The implementer type.</typeparam>
+        /// <param name="serviceName">The service name.</param>
+        /// <param name="life">The life cycle of the implementer type.</param>
+        public static void Register<TService, TImplementer>(string serviceName = null, LifeStyle life = LifeStyle.Singleton)
+            where TService : class
+            where TImplementer : class, TService
+        {
+            Current.Register<TService, TImplementer>(serviceName, life);
+        }
+        /// <summary>Register a implementer type instance as a service implementation.
+        /// </summary>
+        /// <typeparam name="TService">The service type.</typeparam>
+        /// <typeparam name="TImplementer">The implementer type.</typeparam>
+        /// <param name="instance">The implementer type instance.</param>
+        /// <param name="serviceName">The service name.</param>
+        public static void RegisterInstance<TService, TImplementer>(TImplementer instance, string serviceName = null)
+            where TService : class
+            where TImplementer : class, TService
+        {
+            Current.RegisterInstance<TService, TImplementer>(instance, serviceName);
+        }
+        /// <summary>Resolve a service.
+        /// </summary>
+        /// <typeparam name="TService">The service type.</typeparam>
+        /// <returns>The component instance that provides the service.</returns>
+        public static TService Resolve<TService>() where TService : class
+        {
+            return Current.Resolve<TService>();
+        }
+        /// <summary>Resolve a service.
+        /// </summary>
+        /// <param name="serviceType">The service type.</param>
+        /// <returns>The component instance that provides the service.</returns>
+        public static object Resolve(Type serviceType)
+        {
+            return Current.Resolve(serviceType);
+        }
+        /// <summary>Resolve a service.
+        /// </summary>
+        /// <typeparam name="TService">The service type.</typeparam>
+        /// <param name="serviceName">The service name.</param>
+        /// <returns>The component instance that provides the service.</returns>
+        public static TService ResolveNamed<TService>(string serviceName) where TService : class
+        {
+            return Current.ResolveNamed<TService>(serviceName);
+        }
+        /// <summary>Resolve a service.
+        /// </summary>
+        /// <param name="serviceName">The service name.</param>
+        /// <param name="serviceType">The service type.</param>
+        /// <returns>The component instance that provides the service.</returns>
+        public static object ResolveNamed(string serviceName, Type serviceType)
+        {
+            return Current.ResolveNamed(serviceName, serviceType);
+        }
     }
 }

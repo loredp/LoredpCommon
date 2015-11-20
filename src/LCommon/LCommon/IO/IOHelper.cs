a37c576d6d95adf16811377de437252c71c85438
wgli <wgli@Ctrip.com>
2015-11-20 09:10:22 +0800

    LCommon-v1-iohelper update

--- src/LCommon/LCommon/IO/IOHelper.cs
+++ src/LCommon/LCommon/IO/IOHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using LCommon.Logging;
 using LCommon.Utilities;
 
@@ -31,6 +32,16 @@ namespace LCommon.IO
 
         #region private methods
 
+        /// <summary>
+        /// Try io action.
+        /// </summary>
+        /// <param name="actionName"></param>
+        /// <param name="getContext"></param>
+        /// <param name="action"></param>
+        /// <param name="retryTimes"></param>
+        /// <param name="maxRetryTimes"></param>
+        /// <param name="continueRetryWhenRetryFailed"></param>
+        /// <param name="retryInterval"></param>
         private void TryIOActionRecursivelyInternal(string actionName, Func<string> getContext, Action<string, Func<string>, int> action, int retryTimes, int maxRetryTimes, bool continueRetryWhenRetryFailed = false, int retryInterval = 1000)
         {
             try
@@ -42,6 +53,20 @@ namespace LCommon.IO
                 var errorMessage = string.Format("IOException raised when executing action '{0}', currentRetryTimes:{1}, maxRetryTimes:{2}, contextInfo:{3}", actionName, retryTimes, maxRetryTimes, getContext());
 
                 _logger.Error(errorMessage, ex);
+                if (retryTimes >= maxRetryTimes)
+                {
+                    if (!continueRetryWhenRetryFailed)
+                    {
+                        throw;
+                    }
+                    else
+                    {
+                        Thread.Sleep(retryInterval);
+                    }
+                }
+
+                retryTimes++;
+                TryIOActionRecursivelyInternal(actionName, getContext, action, retryTimes, maxRetryTimes);
             }
         }
 

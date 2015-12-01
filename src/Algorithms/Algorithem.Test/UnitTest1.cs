97528f265e63fe3b22da22d5a19c3a6ea39ea89b
wgli <wgli@Ctrip.com>
2015-12-01 20:52:57 +0800

    algorithm-v1-add test project

---
+++ src/Algorithms/Algorithem.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Algorithms.Greedy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithem.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            GreedyBagAlgorithm algorithm = new GreedyBagAlgorithm();
+            algorithm.GetGreedyBagOptimal();
+        }
+    }
+}
